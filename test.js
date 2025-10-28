5e271f15c76dca9107f262921e1b374661df339b
Sneha kumari <snehakumari76373@gmail.com>
2025-10-28 14:41:04 +0530

    change files

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import mult from "./mult.js";
 import sum from "./sum.js";
 
 test("multiplication of 3 and 5 is 15",() => {
-       expect (mult(3,5)).toBe(15);
+       expect (mult(3,5)).toBe(30);
 });
 
 test("sum of 3 and 5 is 8",() => {
